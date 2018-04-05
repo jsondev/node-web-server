75b883ac2c8eee9ebacfab056eae4353df290476
James Anderson <jsondev1.0@outlook.com>
2018-04-04 20:47:27 -0400

    removing maintenance.hbs render

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-    
-})
+
 app.use(express.static(__dirname + '/public'));
 
 // Helpers
